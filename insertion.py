3e6a8610c6e60699c7abc0eb420342c8241af44c
PRAVEEN-Don <117267706+PRAVEEN-Don@users.noreply.github.com>
2024-10-18 14:31:49 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -1,10 +1,20 @@
 def insertion_sort(arr):
+    # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
-        key = arr[i]
+        key = arr[i]  # The element to be inserted
         j = i - 1
 
-        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = key
+        
+        arr[j + 1] = key  # Insert the key at the correct position
+
+# Example usage:
+if __name__ == "__main__":
+    data = [12, 11, 13, 5, 6]
+    print("Unsorted array:", data)
+    insertion_sort(data)
+    print("Sorted array:", data)
